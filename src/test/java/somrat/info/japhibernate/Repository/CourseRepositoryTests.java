2143cae7d6ea41a98c5b0ecfdb3bce5aba4ae2d3
somratcste <somratcste@gmail.com>
2018-12-20 16:10:42 +0600

    writing unit test find by Id method

---
+++ src/test/java/somrat/info/japhibernate/Repository/CourseRepositoryTests.java
@@ -0,0 +1,30 @@
+package somrat.info.japhibernate.Repository;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import somrat.info.japhibernate.Entity.Course;
+import somrat.info.japhibernate.JpaHibernateApplication;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = JpaHibernateApplication.class)
+public class CourseRepositoryTests {
+
+    @Autowired
+    private CourseRepository courseRepository;
+
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @Test
+    public void findById_basic() {
+        Course course = courseRepository.findById(10001L);
+        assertEquals("JPA in 50 Steps", course.getName());
+    }
+
+}
+
