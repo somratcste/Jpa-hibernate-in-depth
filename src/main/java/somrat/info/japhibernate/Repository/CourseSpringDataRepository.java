86e5bd18c81282732a3917b1cde5561165bb2d87
somratcste <somratcste@gmail.com>
2018-12-26 07:30:38 +0600

    spring jpa repository advance

--- src/main/java/somrat/info/japhibernate/Repository/CourseSpringDataRepository.java
+++ src/main/java/somrat/info/japhibernate/Repository/CourseSpringDataRepository.java
@@ -1,8 +1,29 @@
 package somrat.info.japhibernate.Repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import somrat.info.japhibernate.Entity.Course;
+
+import java.util.List;
 
 public interface CourseSpringDataRepository extends JpaRepository<Course, Long> {
 
+    List<Course> findByNameAndId(String name, Long id);
+
+    List<Course> findByName(String name);
+
+    List<Course> countByName(String name);
+
+    List<Course> findByNameOrderByIdDesc(String name);
+
+    List<Course> deleteByName(String name);
+
+    @Query("Select  c  From Course c where name like '%100 Steps'")
+    List<Course> courseWith100StepsInName();
+
+    @Query(value = "Select  *  From Course c where name like '%100 Steps'", nativeQuery = true)
+    List<Course> courseWith100StepsInNameUsingNativeQuery();
+
+    @Query(name = "query_get_100_Step_courses")
+    List<Course> courseWith100StepsInNameUsingNamedQuery();
 }
--- src/test/java/somrat/info/japhibernate/Repository/CourseSpringDataRepositoryTests.java
+++ src/test/java/somrat/info/japhibernate/Repository/CourseSpringDataRepositoryTests.java
@@ -6,6 +6,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit4.SpringRunner;
 import somrat.info.japhibernate.Entity.Course;
@@ -23,6 +27,8 @@ import static org.junit.Assert.*;
 @SpringBootTest(classes = JpaHibernateApplication.class)
 public class CourseSpringDataRepositoryTests {
 
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+
     @Autowired
     private CourseSpringDataRepository courseSpringDataRepository;
 
@@ -36,6 +42,45 @@ public class CourseSpringDataRepositoryTests {
     public void findById_CourseNotPresent() {
         Optional<Course> courseOptional = courseSpringDataRepository.findById(20001L);
         assertFalse(courseOptional.isPresent());
+    }
+
+    @Test
+    public void playingAroundWithSpringDataRepository() {
+        //Course course = new Course("Microservices in 100 Steps");
+        //repository.save(course);
+
+        //course.setName("Microservices in 100 Steps - Updated");
+        //repository.save(course);
+        logger.info("Courses -> {} ", courseSpringDataRepository.findAll());
+        logger.info("Count -> {} ", courseSpringDataRepository.count());
+    }
+
+    @Test
+    public void sort() {
+        Sort sort = new Sort(Sort.Direction.ASC, "name");
+        logger.info("Sorted Courses -> {} ", courseSpringDataRepository.findAll(sort));
+        //Courses -> [Course[JPA in 50 Steps], Course[Spring in 50 Steps], Course[Spring Boot in 100 Steps]]
+    }
+
+    @Test
+    public void pagination() {
+        PageRequest pageRequest = PageRequest.of(0, 3);
+        Page<Course> firstPage = courseSpringDataRepository.findAll(pageRequest);
+        logger.info("First Page -> {} ", firstPage.getContent());
+
+        Pageable secondPageable = firstPage.nextPageable();
+        Page<Course> secondPage = courseSpringDataRepository.findAll(secondPageable);
+        logger.info("Second Page -> {} ", secondPage.getContent());
+    }
+
+    @Test
+    public void findUsingName() {
+        logger.info("FindByName -> {} ", courseSpringDataRepository.findByName("JPA in 50 Steps"));
+    }
+
+    @Test
+    public void findUsingStudentsName() {
+        logger.info("findUsingStudentsName -> {} ", courseSpringDataRepository.findByName("Ranga"));
     }
 }
 
