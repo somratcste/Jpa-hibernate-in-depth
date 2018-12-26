30c861fac006e25e9fa489bb1aaaed2da8281f97
somratcste <somratcste@gmail.com>
2018-12-26 06:31:55 +0600

    criteria query example

---
+++ src/test/java/somrat/info/japhibernate/Repository/CriteriaQueryTest.java
@@ -0,0 +1,165 @@
+package somrat.info.japhibernate.Repository;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import somrat.info.japhibernate.Entity.Course;
+import somrat.info.japhibernate.Entity.Student;
+
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.*;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+
+public class CriteriaQueryTest {
+    @Autowired
+    EntityManager entityManager;
+
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @Test
+    public void all_courses() {
+        // "Select c From Course c"
+
+        // 1. Use Criteria Builder to create a Criteria Query returning the
+        // expected result object
+        CriteriaBuilder cb = entityManager.getCriteriaBuilder();
+        CriteriaQuery<Course> cq = cb.createQuery(Course.class);
+
+        // 2. Define roots for tables which are involved in the query
+        Root<Course> courseRoot = cq.from(Course.class);
+
+        // 3. Define Predicates etc using Criteria Builder
+
+        // 4. Add Predicates etc to the Criteria Query
+
+        // 5. Build the TypedQuery using the entity manager and criteria query
+        TypedQuery<Course> query = entityManager.createQuery(cq.select(courseRoot));
+
+        List<Course> resultList = query.getResultList();
+
+        logger.info("Typed Query -> {}", resultList);
+        // [Course[JPA in 50 Steps], Course[Spring in 50 Steps], Course[Spring
+        // Boot in 100 Steps]]
+    }
+
+    @Test
+    public void all_courses_having_100Steps() {
+        // "Select c From Course c where name like '%100 Steps' "
+
+        // 1. Use Criteria Builder to create a Criteria Query returning the
+        // expected result object
+        CriteriaBuilder cb = entityManager.getCriteriaBuilder();
+        CriteriaQuery<Course> cq = cb.createQuery(Course.class);
+
+        // 2. Define roots for tables which are involved in the query
+        Root<Course> courseRoot = cq.from(Course.class);
+
+        // 3. Define Predicates etc using Criteria Builder
+        Predicate like100Steps = cb.like(courseRoot.get("name"), "%100 Steps");
+
+        // 4. Add Predicates etc to the Criteria Query
+        cq.where(like100Steps);
+
+        // 5. Build the TypedQuery using the entity manager and criteria query
+        TypedQuery<Course> query = entityManager.createQuery(cq.select(courseRoot));
+
+        List<Course> resultList = query.getResultList();
+
+        logger.info("Typed Query -> {}", resultList);
+        // [Course[Spring Boot in 100 Steps]]
+    }
+
+    @Test
+    public void all_courses_without_students() {
+        // "Select c From Course c where c.students is empty"
+
+        // 1. Use Criteria Builder to create a Criteria Query returning the
+        // expected result object
+        CriteriaBuilder cb = entityManager.getCriteriaBuilder();
+        CriteriaQuery<Course> cq = cb.createQuery(Course.class);
+
+        // 2. Define roots for tables which are involved in the query
+        Root<Course> courseRoot = cq.from(Course.class);
+
+        // 3. Define Predicates etc using Criteria Builder
+        Predicate studentsIsEmpty = cb.isEmpty(courseRoot.get("students"));
+
+        // 4. Add Predicates etc to the Criteria Query
+        cq.where(studentsIsEmpty);
+
+        // 5. Build the TypedQuery using the entity manager and criteria query
+        TypedQuery<Course> query = entityManager.createQuery(cq.select(courseRoot));
+
+        List<Course> resultList = query.getResultList();
+
+        logger.info("Typed Query -> {}", resultList);
+        // [Course[Spring in 50 Steps]]
+    }
+
+    @Test
+    public void join() {
+        // "Select c From Course c join c.students s"
+
+        // 1. Use Criteria Builder to create a Criteria Query returning the
+        // expected result object
+        CriteriaBuilder cb = entityManager.getCriteriaBuilder();
+        CriteriaQuery<Course> cq = cb.createQuery(Course.class);
+
+        // 2. Define roots for tables which are involved in the query
+        Root<Course> courseRoot = cq.from(Course.class);
+
+        // 3. Define Predicates etc using Criteria Builder
+        Join<Object, Object> join = courseRoot.join("students");
+
+        // 4. Add Predicates etc to the Criteria Query
+
+        // 5. Build the TypedQuery using the entity manager and criteria query
+        TypedQuery<Course> query = entityManager.createQuery(cq.select(courseRoot));
+
+        List<Course> resultList = query.getResultList();
+
+        logger.info("Typed Query -> {}", resultList);
+        // [Course[JPA in 50 Steps], Course[JPA in 50 Steps], Course[JPA in 50
+        // Steps], Course[Spring Boot in 100 Steps]]
+    }
+
+    @Test
+    public void left_join() {
+        // "Select c From Course c left join c.students s"
+
+        // 1. Use Criteria Builder to create a Criteria Query returning the
+        // expected result object
+        CriteriaBuilder cb = entityManager.getCriteriaBuilder();
+        CriteriaQuery<Course> cq = cb.createQuery(Course.class);
+
+        // 2. Define roots for tables which are involved in the query
+        Root<Course> courseRoot = cq.from(Course.class);
+
+        // 3. Define Predicates etc using Criteria Builder
+        Join<Object, Object> join = courseRoot.join("students", JoinType.LEFT);
+
+        // 4. Add Predicates etc to the Criteria Query
+
+        // 5. Build the TypedQuery using the entity manager and criteria query
+        TypedQuery<Course> query = entityManager.createQuery(cq.select(courseRoot));
+
+        List<Course> resultList = query.getResultList();
+
+        logger.info("Typed Query -> {}", resultList);
+        // [Course[JPA in 50 Steps], Course[JPA in 50 Steps], Course[JPA in 50
+        // Steps], Course[Spring in 50 Steps], Course[Spring Boot in 100 Steps]]
+    }
+
+
+}
